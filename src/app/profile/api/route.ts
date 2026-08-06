af1c3a17e2feee930754cd00a1be815833c68f04
chey134 <giotchey@gmail.com>
2026-08-06 11:35:50 +0400

    Created redirection for route handlers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,24 +1,25 @@
 import { type NextRequest } from "next/server";
-import { headers, cookies } from "next/headers";
+import { headers } from "next/headers";
+import { cookies } from "next/headers";
 
-export async function GET(request: NextRequest){
-    // const requestHeaders = new Headers(request.headers);
-    // console.log(requestHeaders.get("Authorization"));
+export async function GET(request: NextRequest) {
+  const requestHeaders = new Headers(request.headers);
+  console.log(requestHeaders.get("Authorization"));
 
-    const headerList=await headers();
-    console.log(headerList.get("Authorization"));
+  const headersList = await headers();
+  console.log(headersList.get("Authorization"));
 
-    const theme = request.cookies.get("theme");
-    console.log(theme);
+  const theme = request.cookies.get("theme");
+  console.log(theme);
 
-    const cookieStore = await cookies();
-    cookieStore.set("resultsPerPage", "20");
-    console.log(cookieStore.get("resultsPerPage"));
+  const cookieStore = await cookies();
+  cookieStore.set("resultsPerPage", "20");
+  console.log(cookieStore.get("resultsPerPage"));
 
-    return new Response("<h1>Profile API Data!</h1>", {
-        headers:{
-            "Content-Type":"text/html",
-            "Set-Cookie":"theme=dark"
-        }
-    });
-};
+  return new Response("<h1>Profile API data</h1>", {
+    headers: {
+      "Content-Type": "text/html",
+      "Set-Cookie": `theme=dark`,
+    },
+  });
+}
